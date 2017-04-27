d1420cb3108f165301bf4f75ac5eca84faed8701
pantAlex <banpa@web.de>
2017-04-27 12:51:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+var buttonContainer = null;
+var buttonCategories = [];
+var mainIsOn = true;
+
+
+$(document).ready(function() {
+	requestState();
+	
+	buttonContainer = $("#button-container");
+	$('.category').each(function(i, category) {
+		buttonCategories.push($(category));
+	});
+	buttonCategories.splice(0, 1); // Remove main category (first)
+});
+
+function setState(buttonName, state) {
+	buttonName = "#" + buttonName;
+	
+	if (state == "1")
+	{
+		$(buttonName + "On").addClass("selected");
+		$(buttonName + "Off").removeClass("selected");
+	}
+	else
+	{
+		$(buttonName + "Off").addClass("selected");
+		$(buttonName + "On").removeClass("selected");
+	}
+	
+	checkMainCategory();
+}
+
+function requestState() {
+	$.get("state", function(data) {
+		setState("main", data.charAt(0));
+		setState("entrance", data.charAt(1));
+		setState("livingR", data.charAt(2));
+		setState("bathR", data.charAt(3));
+		setState("kitchen", data.charAt(4));
+		setState("bedR", data.charAt(5));
+		
+		setTimeout(requestState, 1000);
+	}, "text").fail(function() {
+		console.log("Request failed.");
+		setTimeout(requestState, 1000);
+	});
+}
+function submitState(room, state) {
+	$.get("submit?room=" + room + "&state=" + state, function(data) {
+		console.log("Submitted new state of " + room + ", sent " + state);
+		setState(room, state);
+	}, "text");
+}
+
+function checkMainCategory() {
+	var mainShouldBeOn = $("#mainOn").hasClass("selected");
+	
+	if (mainShouldBeOn != mainIsOn)
+	{
+		mainIsOn = mainShouldBeOn;
+		
+		if (mainIsOn)
+		{
+			for (var i = 0; i < buttonCategories.length; i++)
+				buttonCategories[i].appendTo(buttonContainer);
+		}
+		else
+		{
+			for (var i = 0; i < buttonCategories.length; i++)
+				buttonCategories[i].detach();
+		}
+	}
+}
